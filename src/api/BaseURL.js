f9da323318ca1550bc5aa0cc5cf31ea0ba228ad2
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2022-06-13 15:42:01 +0300

    base is changed

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,4 +1,4 @@
-const url = "http://ec2-18-207-118-24.compute-1.amazonaws.com:8080/"
+const url = "http://ec2-54-83-74-168.compute-1.amazonaws.com:8080/"
 export default url;
 //https://teklifyap-backend.herokuapp.com/
 //http://localhost:8080/
